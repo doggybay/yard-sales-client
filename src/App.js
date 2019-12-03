8e5d48fc77d7ea96c251cdff7880ce823997f5e3
doggybay <julio.miranda12@gmail.com>
2019-12-02 17:51:57 -0700

    added theme

--- src/App.js
+++ src/App.js
@@ -1,15 +1,34 @@
-import React from 'react';
+import React from 'react'
 import './App.css';
-
+import { ThemeProvider } from '@material-ui/core'
+import { createMuiTheme } from '@material-ui/core/styles'
 import TopNav from './components/layout/TopNav'
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#37474f',
+      dark: '#102027',
+      light: '#62727b',
+      contrastText: '#fff'
+    },
+    secondary: {
+      main: '#0277bd',
+      light: '#58a5f0',
+      dark: '#004c8c',
+      contrastText: '#fff'
+    },
+  }
+});
 
 function App() {
   return (
-    <div className="App">
-      <TopNav />
-      
-    </div>
+    <ThemeProvider theme={theme}>
+      <div className="App">
+        <TopNav />
+      </div>
+    </ThemeProvider>
   );
 }
 
-export default App;
+export default App
--- src/components/layout/TopNav.js
+++ src/components/layout/TopNav.js
@@ -1,54 +1,53 @@
-import React from 'react';
-import { makeStyles, createMuiTheme } from "@material-ui/core/styles";
-import { AppBar, Toolbar, Typography, Button } from '@material-ui/core';
-import purple from '@material-ui/core/colors/purple';
+import React, { Fragment } from 'react';
+import { makeStyles, createMuiTheme } from '@material-ui/core/styles';
+import { AppBar, Toolbar, Typography, Button, ThemeProvider } from '@material-ui/core';
+import teal from '@material-ui/core/colors/teal'
+import cyan from '@material-ui/core/colors/cyan';
 
 
 const useStyles = makeStyles(theme => ({
   root: {
-    flexGrow: 1,
-    background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)'
+    flexGrow: 1
   },
   menuButton: {
     marginRight: theme.spacing(2)
   },
   title: {
     flexGrow: 1
-  }
+  },
+  
 }));
 
 const theme = createMuiTheme({
   palette: {
     primary: {
-      light: "#757ce8",
-      main: "#3f50b5",
-      dark: "#002884",
-      contrastText: "#fff"
+      main: '#37474f',
+      dark: '#102027',
+      light: '#62727b',
+      contrastText: '#fff'
     },
     secondary: {
-      light: "#ff7961",
-      main: "#f44336",
-      dark: "#ba000d",
-      contrastText: "#000"
-    }
+      main: '#0277bd',
+      light: '#58a5f0',
+      dark: '#004c8c',
+      contrastText: '#fff'
+    },
   }
 });
 
 const TopNav = () => {
-  console.log(theme)
-  const classes = useStyles(theme);
+  
+  const classes = useStyles();
 
   return (
-    <div className={classes.root}>
-      <AppBar position="static">
-        <Toolbar>
-          <Typography variant="h6" className={classes.title}>
+      <AppBar color="primary" position="static">
+        <Toolbar >
+          <Typography color="inherit" variant="h6" className={classes.title}>
             Yard Sales
           </Typography>
           <Button color="inherit">Login</Button>
         </Toolbar>
       </AppBar>
-    </div>
   );
 }
 
