7b1d0c9ace1cd43e0e30871d457fca1971ee24ac
Julio Miranda <48833960+doggybay@users.noreply.github.com>
2019-12-04 17:43:00 -0700

    working on styles

--- src/components/layout/TopNav.js
+++ src/components/layout/TopNav.js
@@ -1,16 +1,12 @@
-import React, { Fragment } from 'react';
-import { makeStyles, createMuiTheme } from '@material-ui/core/styles';
-import { AppBar, Toolbar, Typography, Button, ThemeProvider } from '@material-ui/core';
-import teal from '@material-ui/core/colors/teal'
-import cyan from '@material-ui/core/colors/cyan';
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import { AppBar, Toolbar, Typography, Button } from '@material-ui/core';
+
 
 
 const useStyles = makeStyles(theme => ({
   root: {
     flexGrow: 1
-  },
-  menuButton: {
-    marginRight: theme.spacing(2)
   },
   title: {
     flexGrow: 1
@@ -18,36 +14,20 @@ const useStyles = makeStyles(theme => ({
   
 }));
 
-const theme = createMuiTheme({
-  palette: {
-    primary: {
-      main: '#37474f',
-      dark: '#102027',
-      light: '#62727b',
-      contrastText: '#fff'
-    },
-    secondary: {
-      main: '#0277bd',
-      light: '#58a5f0',
-      dark: '#004c8c',
-      contrastText: '#fff'
-    },
-  }
-});
 
 const TopNav = () => {
   
   const classes = useStyles();
 
   return (
-      <AppBar color="primary" position="static">
-        <Toolbar >
-          <Typography color="inherit" variant="h6" className={classes.title}>
-            Yard Sales
-          </Typography>
-          <Button color="inherit">Login</Button>
-        </Toolbar>
-      </AppBar>
+    <AppBar color="primary" position="static">
+      <Toolbar>
+        <Typography color="inherit" variant="h6" className={classes.title}>
+          Yard Sales
+        </Typography>
+        <Button color="inherit">Login</Button>
+      </Toolbar>
+    </AppBar>
   );
 }
 
