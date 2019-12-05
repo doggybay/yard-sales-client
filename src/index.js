434c418c9eedd8979e28ee4e6d1e2f8898049910
Julio Miranda <48833960+doggybay@users.noreply.github.com>
2019-12-04 17:43:29 -0700

    adding providers

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App'
+import { ThemeProvider } from '@material-ui/core'
+
 
 //Redux store
 import store from './store'
 
+//Material UI theme
+import { theme } from './themes/Themes'
+
 //Provider component
 import { Provider } from 'react-redux'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+ReactDOM.render(
+  <Provider store={store}>
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
+  </Provider>,
+  document.getElementById('root'))
