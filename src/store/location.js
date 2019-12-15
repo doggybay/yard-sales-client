4041d10e3b664ceb2b1182fbf544ec8b920997b5
Julio Miranda <48833960+doggybay@users.noreply.github.com>
2019-12-15 09:39:50 -0700

    added new location

--- src/store/location.js
+++ src/store/location.js
@@ -5,6 +5,8 @@ export const local = 'http://localhost:8000/api'
 
 export const homeH = 'http://hypnos:8000/api'
 
-const BASE_URL = LOC !== 'home' ? local : homeH
+export const vpn = 'http://192.168.2.213:/8000/api'
+
+const BASE_URL = LOC !== 'home' ? local : LOC !== 'local' ? vpn : homeH
 
 export default BASE_URL
