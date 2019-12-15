20e36ab2ac6559cef92a040f1ab72e33e01e9d3b
doggybay <julio.miranda12@gmail.com>
2019-12-15 12:01:27 -0700

    location update

--- src/store/location.js
+++ src/store/location.js
@@ -1,12 +1,14 @@
 const LOC = process.env.REACT_APP_LOC
 
 
-export const local = 'http://localhost:8000/api'
+const local = 'http://localhost:8000/api'
 
-export const homeH = 'http://hypnos:8000/api'
+const homeH = 'http://hypnos:8000/api'
 
-export const vpn = 'http://192.168.2.213:/8000/api'
+const vpn = 'http://192.168.2.213:8000/api'
 
-const BASE_URL = LOC !== 'home' ? local : LOC !== 'local' ? vpn : homeH
+const BASE_URL = LOC === 'home' ? homeH : LOC === 'local' ? local : vpn
+
+
 
 export default BASE_URL
