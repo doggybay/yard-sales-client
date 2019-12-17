80ddcf1460f54926ad9bf481fd8bf0c9fe7b3fff
doggybay <julio.miranda12@gmail.com>
2019-12-17 00:27:04 -0700

    adding user reducers

--- src/store/index.js
+++ src/store/index.js
@@ -7,9 +7,10 @@ import { composeWithDevTools } from 'redux-devtools-extension'
 
 //individual reducers
 import sales from './sales/reducer'
+import users from './users/reducer'
 
 //combined reducer
-const rootReducer = combineReducers({ sales })
+const rootReducer = combineReducers({ sales, users })
 
 //middleware array
 const middleware = [thunk, logger]
