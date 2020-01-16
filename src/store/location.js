27ae031352fafa0a3b3cd20052bc7a597906b173
doggybay <julio.miranda12@gmail.com>
2020-01-16 12:20:20 -0700

    location update

--- src/store/location.js
+++ src/store/location.js
@@ -5,9 +5,11 @@ const local = 'http://localhost:8000/api'
 
 const homeH = 'http://hypnos:8000/api'
 
+const homeD = 'http://dionysus:8000/api'
+
 const vpn = 'http://192.168.2.213:8000/api'
 
-const BASE_URL = LOC === 'home' ? homeH : LOC === 'local' ? local : vpn
+const BASE_URL = LOC === 'home' ? homeH : LOC === 'local' ? local : LOC === 'homeD' ? homeD : vpn
 
 
 
